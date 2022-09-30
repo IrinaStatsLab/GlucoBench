607ca868a097fcd98d2371ea3c34da387a2c7fa4
Nicholas Kasman <nck.kasman@gmail.com>
2022-09-30 03:44:54 -0500

    Update subject explorations
    - Moved subject exploration notebooks to exploratory folder
    
    - Updated Dubosson2018 yaml and data formatter files

---
+++ data_formatters/dubosson2018.py
@@ -0,0 +1,68 @@
+# coding=utf-8
+# Copyright 2019 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+# Custom formatting functions for IGLU dataset.
+
+import data_formatters.base
+import data_formatters.utils as utils
+import pandas as pd
+import sklearn.preprocessing
+
+GenericDataFormatter = data_formatters.base.GenericDataFormatter
+DataTypes = data_formatters.base.DataTypes
+InputTypes = data_formatters.base.InputTypes
+
+
+class DUBOSSONFormatter(GenericDataFormatter):
+  # Defines and formats data for the IGLU dataset.
+
+  _column_definition = [
+      ('id', DataTypes.CATEGORICAL, InputTypes.ID),
+      ('time', DataTypes.REAL_VALUED, InputTypes.TIME),
+      ('gl', DataTypes.REAL_VALUED, InputTypes.TARGET) # Glycemic load
+  ]
+
+  _interpolation_params = {
+      'gap_threshold': 45,
+      'min_drop_length': 5
+  }
+
+  def __init__(self):
+    """Initialises formatter."""
+    pass
+  
+  def interpolate(self, df):
+    # TODO: implement interpolation in utils
+    df = utils.interpolate(df, **self._interpolation_params)
+    # create new column with unique id for each subject-segment pair
+    df['segment_id'] = df.id.astype('str') + '_' + df.segment.astype('str')
+    # set subject-segment column as ID and set subject id column as KNOWN_INPUT
+    self._column_definition[0] = ('id', DataTypes.CATEGORICAL, InputTypes.KNOWN_INPUT)
+    self._column_definition += [('segment_id', DataTypes.CATEGORICAL, InputTypes.ID)]
+    return df
+
+  def split_data(self, df):
+    pass
+
+  def set_scalers(self, df):
+    pass
+
+  def transform_inputs(self, df):
+    # TODO: drop subject '009'
+    pass
+
+  def format_predictions(self, df):
+    pass
