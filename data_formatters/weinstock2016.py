78c9bcc881f42692ce955b821696563ece745731
LChun <lizzie_chun1@tamu.edu>
2022-10-13 15:33:57 -0500

    add weinstock dataset with interpolate/split implemented

---
+++ data_formatters/weinstock2016.py
@@ -0,0 +1,110 @@
+# coding=utf-8
+# Copyright 2019 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+# Custom formatting functions for IGLU dataset.
+
+import data_formatters.base
+import data_formatters.utils as utils
+import pandas as pd
+import sklearn.preprocessing
+
+GenericDataFormatter = data_formatters.base.GenericDataFormatter
+DataTypes = data_formatters.base.DataTypes
+InputTypes = data_formatters.base.InputTypes
+
+
+class WeinstockFormatter(GenericDataFormatter):
+  # Defines and formats data for the IGLU dataset.
+
+  _column_definition = [
+      ('id', DataTypes.CATEGORICAL, InputTypes.ID),
+      ('time', DataTypes.REAL_VALUED, InputTypes.TIME),
+      ('gl', DataTypes.REAL_VALUED, InputTypes.TARGET) # Glycemic load
+  ]
+
+  _interpolation_params = {
+    'id_col': 'id',
+    'time_col': 'time',
+    'interpolation_columns': ['gl'], 
+    'constant_columns': [],
+    'gap_threshold': 45, 
+    'min_drop_length': 12, 
+    'interval_length': 5
+  }
+
+  _split_params = {
+  'test_percent_subjects': 0.1,
+  'test_length_segment': 144,
+  'val_length_segment': 144,
+  'min_drop_length': 12,
+  'id_col': 'id',
+  'id_segment_col': 'id_segment',
+}
+
+  _drop_ids = []
+
+  def __init__(self, cnf):
+    """Initialises formatter."""
+    # load parameters from the config file
+    self.params = cnf.all_params
+    # check if data table has index col: -1 if not, index >= 0 if yes
+    self.params['index_col'] = False if self.params['index_col'] == -1 else self.params['index_col']
+    # read data table
+    self.data = pd.read_csv(self.params['data_csv_path'], index_col=self.params['index_col'], na_filter=False)
+    # set time as datetime format in pandas
+    self.data.time = pd.to_datetime(self.data.time)
+    # round time to nearest 5 minutes
+    self.data.time = self.data.time.dt.round('5min') 
+    # within each id, average any duplicates (come from rounding time to 5 minutes)
+    self.data = self.data.groupby(['id', 'time']).mean().reset_index().rename(columns={'index': 'time'})
+
+    # start formatting the data:
+    # 1. drop columns that are not in the column definition
+    # 2. drop rows based on conditions set in the formatter
+    # 3. interpolate missing values
+    # 4. split data into train, val, test
+    # 5. normalize / encode features and targets
+    self.train_idx, self.val_idx, self.test_idx = None, None, None
+    self.drop()
+    self.interpolate()
+    self.split_data()
+
+  def drop(self):
+    # drop columns that are not in the column definition
+    self.data = self.data[[col[0] for col in self._column_definition]]
+    # drop rows based on conditions set in the formatter
+    self.data = self.data.loc[~self.data.id.isin(self._drop_ids)].copy()
+  
+  def interpolate(self):
+    self.data = utils.interpolate(self.data, **self._interpolation_params)
+
+    # create new column with unique id for each subject-segment pair
+    self.data['id_segment'] = self.data.id.astype('str') + '_' + self.data.segment.astype('str')
+    # set subject-segment column as ID and set subject id column as KNOWN_INPUT
+    self._column_definition[0] = ('id', DataTypes.CATEGORICAL, InputTypes.KNOWN_INPUT)
+    self._column_definition += [('id_segment', DataTypes.CATEGORICAL, InputTypes.ID)]
+
+  def split_data(self):
+    self.train_idx, self.val_idx, self.test_idx = utils.split(self.data, **self._split_params)
+
+  def set_scalers(self, df):
+    pass
+
+  def transform_inputs(self, df):
+    pass
+
+  def format_predictions(self, df):
+    pass
--- raw_data/dataset_properties.md
+++ raw_data/dataset_properties.md
@@ -4,3 +4,4 @@
 | [Hall 2018](https://journals.plos.org/plosbiology/article?id=10.1371/journal.pbio.2005143#pbio.2005143.s010) | 57 | tbd | 5 minutes | tbd | 1, 3-5, 7-14 | 16 | 17b |
 | [Dubosson 2018](https://doi.org/10.5281/zenodo.1421615) | 9 | tbd | 5 minutes | tbd | 1-2, 4-5 | 16 | 18b, 20-23 |
 | [Anderson 2016](https://public.jaeb.org/jdrfapp2/stdy/465) | 30 | tbd | 5 minutes | tbd | 1, 4-7, 15 | 16 | 17a, 18a, 19 | 
+| [Weinstock 2016](https://public.jaeb.org/t1dx/stdy/320) | 200 | tbd | 5 minutes | tbd | tbd | 16 | tbd |
