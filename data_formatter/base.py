17c515ad4b33810771effd5c708a3b3c80c0b331
Renat Sergazinov <mrsergazinov@tamu.edu>
2023-01-07 13:22:53 +0600

    Single data fromatter controlled thru config

---
+++ data_formatter/base.py
@@ -0,0 +1,108 @@
+'''Defines a generic data formatter for CGM data sets.'''
+
+import pandas as pd
+import sklearn.preprocessing
+import data_formatter.types as types
+import data_formatter.utils as utils
+
+DataTypes = types.DataTypes
+InputTypes = types.InputTypes
+
+dict_data_type = {'categorical': DataTypes.CATEGORICAL,
+                  'real_valued': DataTypes.REAL_VALUED,
+                  'date': DataTypes.DATE}
+dict_input_type = {'target': InputTypes.TARGET,
+                   'observed_input': InputTypes.OBSERVED_INPUT,      
+                   'known_input': InputTypes.KNOWN_INPUT,
+                   'static_input': InputTypes.STATIC_INPUT,
+                   'id': InputTypes.ID,
+                   'time': InputTypes.TIME}
+
+
+class DataFormatter():
+  # Defines and formats data for the IGLU dataset.
+
+  def __init__(self, cnf):
+    """Initialises formatter."""
+    # load parameters from the config file
+    self.params = cnf
+    
+    # load column definition
+    self.process_column_definition()
+
+    # check that column definition is valid
+    self.check_column_definition()
+
+    # load data
+    # check if data table has index col: -1 if not, index >= 0 if yes
+    self.params['index_col'] = False if self.params['index_col'] == -1 else self.params['index_col']
+    # read data table
+    self.data = pd.read_csv(self.params['data_csv_path'], index_col=self.params['index_col'], na_filter=False)
+
+    # set data types in DataFrame to match column definition
+    self.set_data_types()
+
+    # drop columns / rows
+    self.drop()
+
+    # encode
+    self._encoding_params = self.params['encoding_params']
+    self.encode()
+
+    # interpolate
+    self._interpolation_params = self.params['interpolation_params']
+    self._interpolation_params['interval_length'] = self.params['observation_interval']
+    self.interpolate()
+
+    # split data
+    self._split_params = self.params['split_params']
+    self.split_data()
+
+    # # scale
+    # self.train_data, self.val_data, self.test_data = self.scale()
+
+  def process_column_definition(self):
+    self._column_definition = []
+    for col in self.params['column_definition']:
+      self._column_definition.append((col['name'], 
+                                      dict_data_type[col['data_type']], 
+                                      dict_input_type[col['input_type']]))
+
+  def check_column_definition(self):
+    # check that there is unique ID column
+    assert len([col for col in self._column_definition if col[2] == InputTypes.ID]) == 1, 'There must be exactly one ID column.'
+    # check that there is unique time column
+    assert len([col for col in self._column_definition if col[2] == InputTypes.TIME]) == 1, 'There must be exactly one time column.'
+    # check that there is at least one target column
+    assert len([col for col in self._column_definition if col[2] == InputTypes.TARGET]) >= 1, 'There must be at least one target column.'
+  
+  def set_data_types(self):
+    # set time column as datetime format in pandas
+    for col in self._column_definition:
+      if col[1] == DataTypes.DATE:
+        self.data[col[0]] = pd.to_datetime(self.data[col[0]])
+        # round time to the observation interval
+        self.data[col[0]] = self.data[col[0]].dt.round(self.params['observation_interval'])
+      if col[1] == DataTypes.CATEGORICAL:
+        self.data[col[0]] = self.data[col[0]].astype('category')
+      if col[1] == DataTypes.REAL_VALUED:
+        self.data[col[0]] = self.data[col[0]].astype('float')
+
+  def drop(self):
+    # drop columns that are not in the column definition
+    self.data = self.data[[col[0] for col in self._column_definition]]
+    # drop rows based on conditions set in the formatter
+    for col in self.params['drop'].keys():
+      self.data = self.data.loc[~self.data[col].isin(self.params['drop'][col])].copy()
+  
+  def interpolate(self):
+    self.data, self._column_definition = utils.interpolate(self.data, self._column_definition, **self._interpolation_params)
+
+  def split_data(self):
+    self.train_idx, self.val_idx, self.test_idx = utils.split(self.data, **self._split_params)
+
+  def encode(self):
+    self.data, self._column_definition, self.encoders = utils.encode(self.data, self._column_definition, **self._encoding_params)
+  
+  # def scale(self):
+  #   return utils.scale(self.data, self.train_idx, self.val_idx, self.test_idx)
---
+++ data_formatter/types.py
@@ -0,0 +1,18 @@
+'''Defines data and input types of each column in the dataset.'''
+
+import enum
+
+class DataTypes(enum.IntEnum):
+  """Defines numerical types of each column."""
+  REAL_VALUED = 0
+  CATEGORICAL = 1
+  DATE = 2
+
+class InputTypes(enum.IntEnum):
+  """Defines input types of each column."""
+  TARGET = 0
+  OBSERVED_INPUT = 1
+  KNOWN_INPUT = 2
+  STATIC_INPUT = 3
+  ID = 4  # Single column used as an entity identifier
+  TIME = 5  # Single column exclusively used as a time index
