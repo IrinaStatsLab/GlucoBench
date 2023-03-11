46e65086f54c1024339c30afacad948fda79e31e
mrsergazinov <mrsergazinov>
2023-03-11 12:54:25 -0600

    Linreg single script

--- bin/linreg.py
+++ bin/linreg.py
@@ -3,6 +3,7 @@ import sys
 import os
 import yaml
 import datetime
+import argparse
 from functools import partial
 
 import seaborn as sns
@@ -24,12 +25,14 @@ from data_formatter.base import *
 from bin.utils import *
 
 # define data loader
-def load_data(seed = 0, study_file = None):
+def load_data(seed = 0, study_file = None, dataset = None, use_covs = None):
     # load data
-    with open('./config/weinstock.yaml', 'r') as f:
+    with open(f'./config/{dataset}.yaml', 'r') as f:
         config = yaml.safe_load(f)
     config['split_params']['random_state'] = seed
     formatter = DataFormatter(config, study_file = study_file)
+    assert dataset is not None, 'dataset must be specified in the load_data call'
+    assert use_covs is not None, 'use_covs must be specified in the load_data call'
 
     # convert to series
     time_col = formatter.get_column('time')
@@ -51,26 +54,37 @@ def load_data(seed = 0, study_file = None):
                                     'static': static_cols,
                                     'dynamic': dynamic_cols,
                                     'future': future_cols})
-    
-    # attach static covariates to series
-    for i in range(len(series['train']['target'])):
-        static_covs = series['train']['static'][i][0].pd_dataframe()
-        series['train']['target'][i] = series['train']['target'][i].with_static_covariates(static_covs)
-    for i in range(len(series['val']['target'])):
-        static_covs = series['val']['static'][i][0].pd_dataframe()
-        series['val']['target'][i] = series['val']['target'][i].with_static_covariates(static_covs)
-    for i in range(len(series['test']['target'])):
-        static_covs = series['test']['static'][i][0].pd_dataframe()
-        series['test']['target'][i] = series['test']['target'][i].with_static_covariates(static_covs)
-    for i in range(len(series['test_ood']['target'])):
-        static_covs = series['test_ood']['static'][i][0].pd_dataframe()
-        series['test_ood']['target'][i] = series['test_ood']['target'][i].with_static_covariates(static_covs)
-    
+    if use_covs == 'False':
+        # set dynamic and future covariates to None
+        series['train']['dynamic'] = None
+        series['train']['future'] = None
+        series['val']['dynamic'] = None
+        series['val']['future'] = None
+        series['test']['dynamic'] = None
+        series['test']['future'] = None
+        series['test_ood']['dynamic'] = None
+        series['test_ood']['future'] = None
+    else:
+        # attach static covariates to series
+        for i in range(len(series['train']['target'])):
+            static_covs = series['train']['static'][i][0].pd_dataframe()
+            series['train']['target'][i] = series['train']['target'][i].with_static_covariates(static_covs)
+        for i in range(len(series['val']['target'])):
+            static_covs = series['val']['static'][i][0].pd_dataframe()
+            series['val']['target'][i] = series['val']['target'][i].with_static_covariates(static_covs)
+        for i in range(len(series['test']['target'])):
+            static_covs = series['test']['static'][i][0].pd_dataframe()
+            series['test']['target'][i] = series['test']['target'][i].with_static_covariates(static_covs)
+        for i in range(len(series['test_ood']['target'])):
+            static_covs = series['test_ood']['static'][i][0].pd_dataframe()
+            series['test_ood']['target'][i] = series['test_ood']['target'][i].with_static_covariates(static_covs)
+
     return formatter, series, scalers
 
-def reshuffle_data(formatter, seed):
+def reshuffle_data(formatter, seed, use_covs = None):
     # reshuffle
     formatter.reshuffle(seed)
+    assert use_covs is not None, 'use_covs must be specified in the reshuffle_data call'
 
     # convert to series
     time_col = formatter.get_column('time')
@@ -93,21 +107,43 @@ def reshuffle_data(formatter, seed):
                                     'dynamic': dynamic_cols,
                                     'future': future_cols})
     
-    # attach static covariates to series
-    for i in range(len(series['train']['target'])):
-        static_covs = series['train']['static'][i][0].pd_dataframe()
-        series['train']['target'][i] = series['train']['target'][i].with_static_covariates(static_covs)
-    for i in range(len(series['val']['target'])):
-        static_covs = series['val']['static'][i][0].pd_dataframe()
-        series['val']['target'][i] = series['val']['target'][i].with_static_covariates(static_covs)
-    for i in range(len(series['test']['target'])):
-        static_covs = series['test']['static'][i][0].pd_dataframe()
-        series['test']['target'][i] = series['test']['target'][i].with_static_covariates(static_covs)
-    for i in range(len(series['test_ood']['target'])):
-        static_covs = series['test_ood']['static'][i][0].pd_dataframe()
-        series['test_ood']['target'][i] = series['test_ood']['target'][i].with_static_covariates(static_covs)
+    if use_covs == 'False':
+        # set dynamic and future covariates to None
+        series['train']['dynamic'] = None
+        series['train']['future'] = None
+        series['val']['dynamic'] = None
+        series['val']['future'] = None
+        series['test']['dynamic'] = None
+        series['test']['future'] = None
+        series['test_ood']['dynamic'] = None
+        series['test_ood']['future'] = None
+    else:
+        # attach static covariates to series
+        for i in range(len(series['train']['target'])):
+            static_covs = series['train']['static'][i][0].pd_dataframe()
+            series['train']['target'][i] = series['train']['target'][i].with_static_covariates(static_covs)
+        for i in range(len(series['val']['target'])):
+            static_covs = series['val']['static'][i][0].pd_dataframe()
+            series['val']['target'][i] = series['val']['target'][i].with_static_covariates(static_covs)
+        for i in range(len(series['test']['target'])):
+            static_covs = series['test']['static'][i][0].pd_dataframe()
+            series['test']['target'][i] = series['test']['target'][i].with_static_covariates(static_covs)
+        for i in range(len(series['test_ood']['target'])):
+            static_covs = series['test_ood']['static'][i][0].pd_dataframe()
+            series['test_ood']['target'][i] = series['test_ood']['target'][i].with_static_covariates(static_covs)
     
     return formatter, series, scalers
+
+# lag setter
+def set_lags(in_len, args):
+    lags_past_covariates = None
+    lags_future_covariates = None
+    if args.use_covs == 'True':
+        if series['train']['dynamic'] is not None:
+            lags_past_covariates = in_len
+        if series['train']['future'] is not None:
+            lags_future_covariates = (in_len, formatter.params['length_pred'])
+    return lags_past_covariates, lags_future_covariates
 
 # define objective function
 def objective(trial):
@@ -117,20 +153,24 @@ def objective(trial):
     max_samples_per_ts = trial.suggest_int("max_samples_per_ts", 50, 200, step=50)
     if max_samples_per_ts < 100:
         max_samples_per_ts = None # unlimited
+    lags_past_covariates, lags_future_covariates = set_lags(in_len, args)
 
     # build the Linear Regression model
     model = models.LinearRegressionModel(lags = in_len,
-                                         lags_future_covariates = (in_len, formatter.params['length_pred']),
-                                         output_chunk_length = formatter.params['length_pred'])
+                                        lags_past_covariates = lags_past_covariates,
+                                        lags_future_covariates = lags_future_covariates,
+                                        output_chunk_length = out_len)
 
     # train the model
     model.fit(series['train']['target'],
-            future_covariates=series['train']['future'],
-            max_samples_per_ts=max_samples_per_ts)
+              past_covariates=series['train']['dynamic'],
+              future_covariates=series['train']['future'],
+              max_samples_per_ts=max_samples_per_ts)
 
     # backtest on the validation set
     errors = model.backtest(series['val']['target'],
-                            future_covariates = series['val']['future'],
+                            past_covariates=series['val']['dynamic'],
+                            future_covariates=series['val']['future'],
                             forecast_horizon=out_len,
                             stride=out_len,
                             retrain=False,
@@ -142,48 +182,65 @@ def objective(trial):
 
     return avg_error
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='weinstock')
+parser.add_argument('--use_covs', type=str, default='False')
+parser.add_argument('--optuna', type=str, default='True')
+args = parser.parse_args()
 if __name__ == '__main__':
-    # Optuna study 
-    study_file = './output/linreg_covariates_weinstock.txt'
-    # check that file exists otherwise create it
+    # load data
+    study_file = f'./output/linreg_{args.dataset}.txt' if args.use_covs == 'False' \
+        else f'./output/linreg_covariates_{args.dataset}.txt'
     if not os.path.exists(study_file):
         with open(study_file, "w") as f:
-            # write current date and time
             f.write(f"Optimization started at {datetime.datetime.now()}\n")
-    # load data
-    formatter, series, scalers = load_data(study_file=study_file)
-    study = optuna.create_study(direction="minimize")
-    print_call = partial(print_callback, study_file=study_file)
-    study.optimize(objective, n_trials=50, 
-                   callbacks=[print_call], 
-                   catch=(np.linalg.LinAlgError, KeyError))
+    formatter, series, scalers = load_data(study_file=study_file, 
+                                           dataset=args.dataset,
+                                           use_covs=args.use_covs)
     
-    # Select best hyperparameters
-    best_params = study.best_trial.params
+    # hyperparameter optimization
+    best_params = None
+    if args.optuna == 'True':
+        study = optuna.create_study(direction="minimize")
+        print_call = partial(print_callback, study_file=study_file)
+        study.optimize(objective, n_trials=50, 
+                    callbacks=[print_call], 
+                    catch=(np.linalg.LinAlgError, KeyError))
+        best_params = study.best_trial.params
+    else:
+        key = "linreg_covariates" if args.use_covs == 'True' else "linreg"
+        assert formatter.params[key] is not None, "No saved hyperparameters found for this model"
+        best_params = formatter.params[key]
+
+    # select best hyperparameters
     in_len = best_params['in_len']
     out_len = formatter.params["length_pred"]
     stride = out_len // 2
     max_samples_per_ts = best_params['max_samples_per_ts']
     if max_samples_per_ts < 100:
         max_samples_per_ts = None # unlimited
+    lags_past_covariates, lags_future_covariates = set_lags(in_len, args)
 
-    # Test on ID and OOD data
+    # test on ID and OOD data
     seeds = list(range(10, 20))
     id_errors_stats = {'mean': [], 'std': [], 'quantile25': [], 'quantile75': [], 'median': [], 'min': [], 'max': []}
     ood_errors_stats = {'mean': [], 'std': [], 'quantile25': [], 'quantile75': [], 'median': [], 'min': [], 'max': []}
     for seed in seeds:
-        formatter, series, scalers = reshuffle_data(formatter, seed)
+        formatter, series, scalers = reshuffle_data(formatter, seed, use_covs=args.use_covs)
         # build the model
         model = models.LinearRegressionModel(lags = in_len,
-                                             lags_future_covariates = (in_len, formatter.params['length_pred']),
+                                             lags_past_covariates = lags_past_covariates,
+                                             lags_future_covariates = lags_future_covariates,
                                              output_chunk_length = formatter.params['length_pred'])
         # train the model
         model.fit(series['train']['target'],
+                  past_covariates=series['train']['dynamic'],
                   future_covariates=series['train']['future'],
                   max_samples_per_ts=max_samples_per_ts)
 
         # backtest on the test set
         forecasts = model.historical_forecasts(series['test']['target'],
+                                               past_covariates = series['test']['dynamic'],
                                                future_covariates = series['test']['future'],
                                                forecast_horizon=out_len, 
                                                stride=stride,
@@ -192,10 +249,10 @@ if __name__ == '__main__':
                                                last_points_only=False,
                                                start=formatter.params["max_length_input"])
         id_errors_sample = rescale_and_backtest(series['test']['target'],
-                                    forecasts,  
-                                    [metrics.mse, metrics.mae],
-                                    scalers['target'],
-                                    reduction=None)
+                                                forecasts,  
+                                                [metrics.mse, metrics.mae],
+                                                scalers['target'],
+                                                reduction=None)
         id_errors_sample = np.vstack(id_errors_sample)
         id_error_stats_sample = compute_error_statistics(id_errors_sample)
         for key in id_errors_stats.keys():
@@ -205,6 +262,7 @@ if __name__ == '__main__':
 
         # backtest on the ood test set
         forecasts = model.historical_forecasts(series['test_ood']['target'],
+                                               past_covariates = series['test_ood']['dynamic'],
                                                future_covariates = series['test_ood']['future'],
                                                forecast_horizon=out_len, 
                                                stride=stride,
@@ -213,10 +271,10 @@ if __name__ == '__main__':
                                                last_points_only=False,
                                                start=formatter.params["max_length_input"])
         ood_errors_sample = rescale_and_backtest(series['test_ood']['target'],
-                                    forecasts,  
-                                    [metrics.mse, metrics.mae],
-                                    scalers['target'],
-                                    reduction=None)
+                                                 forecasts,  
+                                                 [metrics.mse, metrics.mae],
+                                                 scalers['target'],
+                                                 reduction=None)
         ood_errors_sample = np.vstack(ood_errors_sample)
         ood_errors_stats_sample = compute_error_statistics(ood_errors_sample)
         for key in ood_errors_stats.keys():
---
+++ bin/linreg.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# execute in parallel
+nohup python ./bin/linreg.py --dataset weinstock --use_covs False --optuna False > ./output/track_linreg_weinstock.txt &
+nohup python ./bin/linreg.py --dataset weinstock --use_covs True --optuna False > ./output/track_linreg_covariates_weinstock.txt &
+nohup python ./bin/linreg.py --dataset colas --use_covs False --optuna False > ./output/track_linreg_colas.txt &
+nohup python ./bin/linreg.py --dataset colas --use_covs True --optuna False > ./output/track_linreg_covariates_colas.txt &
+nohup python ./bin/linreg.py --dataset dubosson --use_covs False --optuna True > ./output/track_linreg_dubosson.txt &
+nohup python ./bin/linreg.py --dataset dubosson --use_covs True --optuna True > ./output/track_linreg_covariates_dubosson.txt &
+nohup python ./bin/linreg.py --dataset hall --use_covs False --optuna False > ./output/track_linreg_hall.txt &
+nohup python ./bin/linreg.py --dataset hall --use_covs True --optuna False > ./output/track_linreg_covariates_hall.txt &
+nohup python ./bin/linreg.py --dataset iglu --use_covs False --optuna False > ./output/track_linreg_iglu.txt &
+nohup python ./bin/linreg.py --dataset iglu --use_covs True --optuna False > ./output/track_linreg_covariates_iglu.txt &
