5efdec272c7efbba6ad47ff8b75917c723638467
mrsergazinov <mrsergazinov>
2023-03-22 17:37:11 -0500

    Split files into folders

--- bin/arima.sh
+++ bin/arima.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # execute in parallel
-nohup python ./bin/arima.py --dataset weinstock > ./output/track_arima_weinstock.txt &
-nohup python ./bin/arima.py --dataset colas > ./output/track_arima_colas.txt &
-nohup python ./bin/arima.py --dataset dubosson > ./output/track_arima_dubosson.txt &
-nohup python ./bin/arima.py --dataset hall > ./output/track_arima_hall.txt &
-nohup python ./bin/arima.py --dataset iglu > ./output/track_arima_iglu.txt &
+nohup python ./lib/arima.py --dataset weinstock > ./output/track_arima_weinstock.txt &
+nohup python ./lib/arima.py --dataset colas > ./output/track_arima_colas.txt &
+nohup python ./lib/arima.py --dataset dubosson > ./output/track_arima_dubosson.txt &
+nohup python ./lib/arima.py --dataset hall > ./output/track_arima_hall.txt &
+nohup python ./lib/arima.py --dataset iglu > ./output/track_arima_iglu.txt &
 
--- bin/transformer.sh
+++ bin/transformer.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # execute in parallel
-# nohup python ./bin/transformer.py --dataset weinstock --use_covs False --optuna False > ./output/track_transformer_weinstock.txt &
-nohup python ./bin/transformer.py --dataset weinstock --use_covs True --optuna False > ./output/track_transformer_covariates_weinstock.txt &
-# nohup python ./bin/transformer.py --dataset colas --use_covs False --optuna False > ./output/track_transformer_colas.txt &
-nohup python ./bin/transformer.py --dataset colas --use_covs True --optuna False > ./output/track_transformer_covariates_colas.txt &
-# nohup python ./bin/transformer.py --dataset dubosson --use_covs False --optuna False > ./output/track_transformer_dubosson.txt &
-nohup python ./bin/transformer.py --dataset dubosson --use_covs True --optuna False > ./output/track_transformer_covariates_dubosson.txt &
-# nohup python ./bin/transformer.py --dataset hall --use_covs False --optuna False > ./output/track_transformer_hall.txt &
-nohup python ./bin/transformer.py --dataset hall --use_covs True --optuna False > ./output/track_transformer_covariates_hall.txt &
-# nohup python ./bin/transformer.py --dataset iglu --use_covs False --optuna False > ./output/track_transformer_iglu.txt &
-nohup python ./bin/transformer.py --dataset iglu --use_covs True --optuna False > ./output/track_transformer_covariates_iglu.txt &
+nohup python ./lib/transformer.py --dataset weinstock --use_covs False --optuna False > ./output/track_transformer_weinstock.txt &
+nohup python ./lib/transformer.py --dataset weinstock --use_covs True --optuna False > ./output/track_transformer_covariates_weinstock.txt &
+nohup python ./lib/transformer.py --dataset colas --use_covs False --optuna False > ./output/track_transformer_colas.txt &
+nohup python ./lib/transformer.py --dataset colas --use_covs True --optuna False > ./output/track_transformer_covariates_colas.txt &
+nohup python ./lib/transformer.py --dataset dubosson --use_covs False --optuna False > ./output/track_transformer_dubosson.txt &
+nohup python ./lib/transformer.py --dataset dubosson --use_covs True --optuna False > ./output/track_transformer_covariates_dubosson.txt &
+nohup python ./lib/transformer.py --dataset hall --use_covs False --optuna False > ./output/track_transformer_hall.txt &
+nohup python ./lib/transformer.py --dataset hall --use_covs True --optuna False > ./output/track_transformer_covariates_hall.txt &
+nohup python ./lib/transformer.py --dataset iglu --use_covs False --optuna False > ./output/track_transformer_iglu.txt &
+nohup python ./lib/transformer.py --dataset iglu --use_covs True --optuna False > ./output/track_transformer_covariates_iglu.txt &
