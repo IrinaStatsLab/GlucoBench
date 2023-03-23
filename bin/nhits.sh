76307a79deeed80f0a5281f56c1d19a690a65218
mrsergazinov <mrsergazinov>
2023-03-23 15:28:36 -0500

    Added optimal hyperparams for nhits to config

--- bin/nhits.sh
+++ bin/nhits.sh
@@ -6,8 +6,8 @@ nohup python ./lib/nhits.py --dataset weinstock --use_covs True --optuna False >
 nohup python ./lib/nhits.py --dataset colas --use_covs False --optuna False > ./output/track_nhits_colas.txt &
 nohup python ./lib/nhits.py --dataset colas --use_covs True --optuna False > ./output/track_nhits_covariates_colas.txt &
 nohup python ./lib/nhits.py --dataset dubosson --use_covs False --optuna False > ./output/track_nhits_dubosson.txt &
-nohup python ./lib/nhits.py --dataset dubosson --use_covs True --optuna True > ./output/track_nhits_covariates_dubosson.txt &
+nohup python ./lib/nhits.py --dataset dubosson --use_covs True --optuna False > ./output/track_nhits_covariates_dubosson.txt &
 nohup python ./lib/nhits.py --dataset hall --use_covs False --optuna False > ./output/track_nhits_hall.txt &
 nohup python ./lib/nhits.py --dataset hall --use_covs True --optuna False > ./output/track_nhits_covariates_hall.txt &
 nohup python ./lib/nhits.py --dataset iglu --use_covs False --optuna False > ./output/track_nhits_iglu.txt &
-nohup python ./lib/nhits.py --dataset iglu --use_covs True --optuna True > ./output/track_nhits_covariates_iglu.txt &
+nohup python ./lib/nhits.py --dataset iglu --use_covs True --optuna False > ./output/track_nhits_covariates_iglu.txt &
